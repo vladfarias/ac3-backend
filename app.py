f80511c41e6d410ca510bdbbf7d71d38952df09a
vladfarias <vladimir.farias@avanade.com>
2023-05-01 09:52:22 -0300

    feat: created backend to solve ac3

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import uuid
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/home', methods=['GET'])
+def home():
+    return 'Esta é a tela HOME 🏚️'
+
+@app.route('/contato', methods=['POST'])
+def contato():
+    with open('data.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    reclamacao = request.get_json()
+    reclamacao['id'] = str(uuid.uuid4())
+    data.append(reclamacao)
+
+    with open('data.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False)
+
+    return 'Reclamação recebida! Agurade nosso contato'
+
+if __name__ =='__main__':
+    app.run(port=3000, debug=True)
